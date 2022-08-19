1cff2f9bd306a111f8555f62951e1c5a8abbbd13
Thiago H. Deicke <thiago@xptec.com.br>
2022-08-19 18:01:03 -0300

    teste

--- batch_ssh.bat
+++ batch_ssh.bat
@@ -7,5 +7,13 @@ echo %commit_msg%
 cd "C:\Users\xpert\OneDrive\XPERT\Deicke\GitLab\XPtec\Firmware\mt76_deicke" 
 git add --all
 git commit -m "%commit_msg%"
+git push 
+
+git log -n 1 --date=short --pretty=format:'PKG_SOURCE_VERSION:=%%H%%n' > %HASH%
+git log -n 1 --date=short --pretty=format:'PKG_SOURCE_DATE:=%%ad%%n' > %DATA%
+
+echo %HASH%
+echo %DATA%
+
 
 pause
