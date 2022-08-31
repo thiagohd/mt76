382e8b0262c0a829ded8798fcc173ef03da9b021
Thiago H. Deicke <thiago@xptec.com.br>
2022-08-31 16:05:36 -0300

    teste

--- mt7603/mcu.c
+++ mt7603/mcu.c
@@ -438,7 +438,7 @@ int mt7603_mcu_set_eeprom(struct mt7603_dev *dev)
 
 			if(data[i].addr == 0x0024) data[i].val = 0x0000; //	??
 			if(data[i].addr == 0x0025) data[i].val = 0x0000; //	??
-			if(data[i].addr == 0x0034) data[i].val = 0x0022; //0x0022; //	NIC_CONFG_00
+			if(data[i].addr == 0x0034) data[i].val = 0x0011; //0x0022; //	NIC_CONFG_00
 
 			// Aparentemente mudando o terceiro bit para 1 liga o EXT_PA
 			if(data[i].addr == 0x0035) data[i].val = 0x0037; //0x0034; //	NIC_CONFG_01 (EXT_PA) MUDANDO PARA 0XFF AUMENTA POTENCIA 
